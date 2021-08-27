813a8b65d913cf5198227ed68b0af34671a0686e
Varun Yadav <varunyadav155@gmail.com>
2021-08-27 12:00:30 +0530

    CodingBat Solutions

---
+++ Python/String_2/06_xyz_there_Solution.py
@@ -0,0 +1,10 @@
+def xyz_there(str):
+
+    for i in range (len(str)-2):
+      if str[i] != '.' and str[i+1:i+4] == 'xyz':
+        return True
+     
+    if str[0:3] == 'xyz':
+      return True
+      
+    return False
